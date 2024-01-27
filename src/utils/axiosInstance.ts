a1b95b280f48c6918745c092758669dafeccf857
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-01-27 06:15:43 +0200

    deploy

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const apiUrlV1 = axios.create({
+  baseURL: "https://safari-ride-production.up.railway.app",
+});
+const apiUrlV1 = axios.create({
   baseURL: "http://localhost:4001/api/v1",
 });
 
